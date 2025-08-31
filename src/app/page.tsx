16f3bf2613cfbcf9552b22302385016ac3b09536
shellagroup2030-droid <shellagroup2030@gmail.com>
2025-08-31 17:15:45 +0300

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import LandingPage from "@/components/LandingPage/landingpage";
 import Navbar from "@/components/navbar";
 import Shellafooter from "@/components/shellafooter";
+import { Analytics } from "@vercel/analytics/next"
 export default function Home() {
   return (
     <div>
