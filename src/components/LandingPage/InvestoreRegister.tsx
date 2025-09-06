1f1cd0f8773c5c8d4fa6b596ac032e704c6d12f8
shellagroup2030-droid <shellagroup2030@gmail.com>
2025-09-06 20:20:17 +0300

    deployready

--- src/components/LandingPage/InvestoreRegister.tsx
+++ src/components/LandingPage/InvestoreRegister.tsx
@@ -19,7 +19,7 @@ const Notification = ({
 	onClose: () => void;
 }) => {
 	if (!isVisible) return null;
-
+		
 	return (
 		<div className="fixed inset-0 z-50 flex items-center justify-center p-4">
 			<div
